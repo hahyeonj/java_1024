5c79d8029b145ae83f9b9c24bfab7cfe83b0656b
luneaa <rkrdh727@naver.com>
2022-10-26 21:49:47 +0900

    자바 03일차 : For 문 구구단

---
+++ java_jhh/src/ForEx09.java
@@ -0,0 +1,17 @@
+
+public class ForEx09 {
+
+	public static void main(String[] args) {
+	
+		int i ;
+		int k ;
+		System.out.println("구구단");
+		for( i = 2, k = 1 ; i <= 9; i++) {
+			System.out.println( i + "단");
+		
+				
+			System.out.println( i +" * "+ k + " = " + (i * k) );
+		}
+	}
+
+}
