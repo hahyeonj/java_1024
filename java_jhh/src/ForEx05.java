4289027f26c3d01daa34a8fff3123c38b604351d
luneaa <rkrdh727@naver.com>
2022-10-26 21:49:23 +0900

    자바 03일차 : For 예제

---
+++ java_jhh/src/ForEx05.java
@@ -0,0 +1,25 @@
+
+public class ForEx05 {
+
+	public static void main(String[] args) {
+		/* 1부터 10까지의 합을 구하는 코드를 작성하세요.
+		 * 반복횟수 : i는 1부터 10까지 1씩 증가
+		 * 규칙성 : ??
+		 * 		 sum0 = 0
+		 * i = 1 sum1 = sum+1
+		 * i = 2 sum2 = sum+2
+		 * i = 3 sum3 = sum+3
+		 * i = 4 sum4 = sum+4
+		 * ... 
+		 * i = 10 sum=sum+10
+		 * */
+		int i, k;
+		for(i = 0 ; i <= 10 ; i++) {
+			System.out.println(k = i + i++);
+		}
+		
+		
+		
+	}
+
+}
